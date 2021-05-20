f0ef2591b64736f569287b9e6d363b0bb5710ea5
Nikhil Verma <nikhil.verma@cldcvr.com>
2021-05-21 03:35:14 +0530

    Disable extension of vue stores

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -7,12 +7,12 @@ import { Store } from "./index";
 
 declare module "vue/types/options" {
   interface ComponentOptions<V extends Vue> {
-    store?: Store<any>;
+    // store?: Store<any>;
   }
 }
 
 declare module "vue/types/vue" {
   interface Vue {
-    $store: Store<any>;
+    // $store: Store<any>;
   }
 }
